92ad28e65d2a8690d39fbe1d43732046eb4c1761
Karim Kameka <kamekk@hotmail.com>
2020-12-11 12:41:50 -0500

    Get Locations Complete

--- src/GetAzureLocations/run.ps1
+++ src/GetAzureLocations/run.ps1
@@ -1,11 +1,11 @@
 using namespace System.Net
+using namespace System.Collections
 
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
 # Write to the Azure Functions log stream.
 Write-Host "Getting Azure Locations for Subscription..."
-
 $subscriptionId = $env:subscriptionId
 $tenantId = $env:tenantId
 
@@ -14,9 +14,19 @@ Write-Host "TenantId: " + $tenantId
 
 ###################################
 #Set Default Security
-Set-AzContext -Subscription "$subscriptionId"
+Set-AzContext -Subscription $subscriptionId
 
-$locations = Get-AzureRmLocation
+$response = Get-AzLocation
+
+$locations = @()
+
+foreach ($item in $response) {
+    $newLocation = @{
+                     location=$item.Location
+                     displayName=$item.DisplayName
+                    }
+    $locations += $newLocation
+}
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
